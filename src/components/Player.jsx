95916e42865fbdcceb8ceccca2e81d0ae73b10dc
Sabrina Kuah <sabrina@Sabrinas-MacBook-Pro.local>
2020-12-29 20:42:41 -0800

    Player components and styling added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Body from "./Body";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
 
 function Player({ spotify }) {
-  return <div>I am the player</div>;
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Player;
